bf4153c34f4791d7c6d9fc5954805a27e478d9a3
manosteele91827 <Cam.steeleis@gmail.com>
2017-03-30 20:12:53 -0500

    Fixed some things

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+/*
+* Cameron Steele
+* Github: manosteele91827
+*
+* A Direct P5.js port of ceriwestcott's ConnectedLines project
+*
+* Based Off: https://github.com/ceriwestcott/ConnectedLines
+* By ceriwestcott
+*/
 var ring = [];
 var looping = true;
 var distance = 100;
 var ds = 0.2;
+var colr = function(start,end,use){
+  this.val = start;
+  this.end = end;
+  this.next = function(){
+    if(this.val === this.end){
+      this.val = 1;
+    }else{
+      this.val++;
+    }
+  };
+  this.maybe = function(){
+    if(random(0,1)){
+      this.next();
+    }
+  }; 
+  
+}
+
+var c1 = new colr(255,255,false);
+var c2 = new colr(200,200,false);
+
 
 function setup() {
   createCanvas(800,600);
@@ -37,9 +67,10 @@ function draw() {
       if(dist(ring[i].pos.x,ring[i].pos.y,ring[j].pos.x,ring[j].pos.y) < distance){
         var dis = dist(ring[i].pos.x,ring[i].pos.y,ring[j].pos.x,ring[j].pos.y);
         var alph = map(dis,distance,0,0,255);
-        
         strokeWeight(2);
-        stroke(255,alph,200,alph);
+        stroke(c1.val, alph, c2.val,alph);
+        c1.next();
+        c2.maybe();
         line(ring[i].pos.x,ring[i].pos.y,ring[j].pos.x,ring[j].pos.y);
       }
     }  
